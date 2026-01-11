f4ee4af3e7d43e1ab275357366b93d21f99c4bf0
Hoai Nam <hnamz.dev@gmail.com>
2026-01-12 00:12:43 +0700

    Exercise of lesson 23.1

--- js/2026-01-06/main.js
+++ js/2026-01-06/main.js
@@ -1,13 +1,13 @@
 //Lesson 23
 //1
-const classifyTriangle = (a, b, c) => {
+const getTriangleType = (a, b, c) => {
     if (a + b > c && a + c > b && b + c > a){
         if(a===b && b===c){
-            return "Equilateral Triangle"
+            return "Equilateral"
         }else if(a*a === b*b + c*c || b*b === a*a + c*c || c*c === b*b + a*a){
-            return "Right Triangle"
+            return "Right"
         }else if(a === b || b === c || a === c){
-            return "Isosceles Triangle"
+            return "Isosceles"
         }else{
             return "Triangle"
         }
@@ -15,21 +15,21 @@ const classifyTriangle = (a, b, c) => {
         return false
     }
 }
-let result = classifyTriangle(3, 3, 3)
+let result = getTriangleType(3, 3, 3)
 console.log(result)
 
 //2
 const isPerfectSquare = (n) => {
     if(typeof n !== "number" || n < 0){
-        return "Invalid number"
+        return false
     }
     if(n=== (n**(1/2))*(n**(1/2))){
-      return `${n} is a perfect square`
+       return  true
     }else {
-      return `${n} is not a perfect square`
+        return false
     }
 }
-let result2 = isPerfectSquare(9)
-console.log(result2)
 
+let n = 9
+console.log(isPerfectSquare(n)?`${n} is a perfect square`: `${n} is not a perfect square`)
 
