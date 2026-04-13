7f8caf4846d3908820766248e7dc7d90786c0fc9
Hoai Nam <hnamz.dev@gmail.com>
2026-04-13 22:46:52 +0700

    Exercise of lesson 40.1.1

--- react/2026-04-07/src/Table.jsx
+++ react/2026-04-07/src/Table.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 export default () => {
 
     const columns = [
@@ -48,24 +50,30 @@ export default () => {
         }
     ]
 
-    const students = [
-        { id: 1, name: "Nguyen Van A", age: 15, class: "10A1", address: "Ha Noi" },
-        { id: 2, name: "Tran Thi B", age: 16, class: "10A2", address: "Hai Phong" },
-        { id: 3, name: "Le Van C", age: 15, class: "10A1", address: "Da Nang" },
-        { id: 4, name: "Pham Thi D", age: 17, class: "11A1", address: "Ha Noi" },
-        { id: 5, name: "Hoang Van E", age: 16, class: "10A3", address: "Nam Dinh" },
-        { id: 6, name: "Do Thi F", age: 15, class: "10A2", address: "Thai Binh" },
-        { id: 7, name: "Bui Van G", age: 17, class: "11A2", address: "Hai Duong" },
-        { id: 8, name: "Vu Thi H", age: 16, class: "10A3", address: "Ha Noi" },
-        { id: 9, name: "Dang Van I", age: 15, class: "10A1", address: "Bac Ninh" },
-        { id: 10, name: "Ngo Thi K", age: 17, class: "11A1", address: "Ha Nam" }
-    ];
+    const [students, setStudents] = useState(
+        [
+            { id: 1, name: "Nguyen Van A", age: 15, class: "10A1", address: "Ha Noi" },
+            { id: 2, name: "Tran Thi B", age: 16, class: "10A2", address: "Hai Phong" },
+            { id: 3, name: "Le Van C", age: 15, class: "10A1", address: "Da Nang" },
+            { id: 4, name: "Pham Thi D", age: 17, class: "11A1", address: "Ha Noi" },
+            { id: 5, name: "Hoang Van E", age: 16, class: "10A3", address: "Nam Dinh" },
+            { id: 6, name: "Do Thi F", age: 15, class: "10A2", address: "Thai Binh" },
+            { id: 7, name: "Bui Van G", age: 17, class: "11A2", address: "Hai Duong" },
+            { id: 8, name: "Vu Thi H", age: 16, class: "10A3", address: "Ha Noi" },
+            { id: 9, name: "Dang Van I", age: 15, class: "10A1", address: "Bac Ninh" },
+            { id: 10, name: "Ngo Thi K", age: 17, class: "11A1", address: "Ha Nam" }
+        ]);
 
     const onEdit =(id) => {
         console.log(id)
     }
     const onDelete =(id) => {
         console.log(id)
+        const confirm = window.confirm("Ban co muon xoa khong?")
+        if(confirm){
+            setStudents((prevStudents)=>prevStudents.filter((students)=>students.id !==id))
+        }
+        return;
     }
     return (
         <table width='100%' cellPadding={0} cellSpacing={0} border={1}>
